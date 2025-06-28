d7e4fbaa194dc700be4fdd86f1c6675328a39ed3
emondewan00 <dewanmohammademon@gmail.com>
2025-06-29 04:34:35 +0600

    fix: remove axios to get token

--- src/lib/getToken.ts
+++ src/lib/getToken.ts
@@ -1,9 +1,10 @@
-import axiosClient from "./axios";
-
 const getToken = async () => {
   const url = process.env.NEXT_PUBLIC_FRONTEND_BASE_URL;
-  const res = await axiosClient.get(url + "/api/token");
-  return res.data.token;
+
+  const response = await fetch(`${url}/api/token`);
+  const data = await response.json();
+
+  return data.token;
 };
 
 export default getToken;
