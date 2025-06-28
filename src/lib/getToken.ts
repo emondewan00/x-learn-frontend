229bcdc71c70e68ad2a87b391059a91190430f91
emondewan00 <dewanmohammademon@gmail.com>
2025-06-29 04:28:23 +0600

    fix: base url add for get token utility function

--- src/lib/getToken.ts
+++ src/lib/getToken.ts
@@ -1,7 +1,8 @@
 import axiosClient from "./axios";
 
 const getToken = async () => {
-  const res = await axiosClient.get("/api/token");
+  const url = process.env.NEXT_PUBLIC_FRONTEND_BASE_URL;
+  const res = await axiosClient.get(url + "/api/token");
   return res.data.token;
 };
 
